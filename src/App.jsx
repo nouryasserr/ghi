bf3590d6d085ecf62940314df8fc4518b31eee8d
nouryasserr <nouryasser18113@gmail.com>
2026-02-27 22:48:46 +0200

    fix path case issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import About from "./pages/About/About";
 import Layout from "./components/Layout/Layout";
 import Contact from "./pages/Contact/Contact";
 import Projects from "./pages/Projects/Projects";
-import Solutions from "./pages/Solutions/Solutions";
 import Technology from "./pages/Technology/Technology";
 import Wallformwork from "./pages/Wallformwork/Wallformwork";
 import Columnformwork from "./pages/Columnformwork/Columnformwork";
 import Climbing from "./pages/Climbing/Climbing";
 import Slab from "./pages/Slab/Slab";
-import Shoring from "./pages/Shoring/Shoring";
+import Shoringsystem from "./pages/Shoringsystem/Shoringsystem";
 
 function App() {
   const router = createBrowserRouter([
@@ -25,13 +24,12 @@ function App() {
         },
         { path: "/contact", element: <Contact /> },
         { path: "/projects", element: <Projects /> },
-        { path: "/solutions", element: <Solutions /> },
         { path: "/technology", element: <Technology /> },
         { path: "/wallformwork", element: <Wallformwork /> },
         { path: "/columnformwork", element: <Columnformwork /> },
         { path: "/climbingsystems", element: <Climbing /> },
-        { path: "/shoring", element: <Shoring /> },
         { path: "/slabsupport", element: <Slab /> },
+        { path: "/shoring", element: <Shoringsystem /> },
       ],
     },
   ]);
